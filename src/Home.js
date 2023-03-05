b6b39d55df96920ebf4b2085c9fa36ce4084eb36
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-03-05 16:26:25 +0530

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -5,29 +5,29 @@ import Career from "./Career"
 import About from "./About"
 
 
-function Links(){
-  return(
-    <div>
-      <ul>
-        <li><Link to="/Career">Career</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/">Log out</Link></li>
-      </ul>
-    </div>
-  );
-}
+// function Links(){
+//   return(
+//     <div>
+//       <ul>
+//         <li><Link to="/Career">Career</Link></li>
+//         <li><Link to="/About">About</Link></li>
+//         <li><Link to="/">Log out</Link></li>
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default function Home() {
   return (
     <div>
-      {Links()}
+      {/* {Links()} */}
       <h1>Successfully Logged in,This is home page</h1>
 
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Career" element={<Career />} />
         <Route path="/About" element={<About />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
