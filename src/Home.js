cb67e98d010e9d33e1f6bed85bf328fe3ef58cbf
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-03-10 09:53:28 +0530

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import App from "./App"
 
 // function logout()
 // {
-//   // setIsLogin(false);
-//   // navigate('/Sign in');
 //   localStorage.clear();
+//   App()
 
 // }
 
@@ -20,9 +20,9 @@ export default function Home() {
         <li><Link to="/Career">Career</Link></li>
         <li><Link to="/About">About</Link></li>
         <li><Link to="/Sign in">Log out</Link></li> 
+        {/* <li><Link onClick={logout}>Log out</Link></li>  */}
         {/* <li><Link to="/Sign in" onClick={logout}>Log out</Link></li>  */}
       </ul>
-
       
     </div>
   );
