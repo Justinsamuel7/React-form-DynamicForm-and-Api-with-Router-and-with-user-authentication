11cb8720b1eec90438f033579e669f1f2c831796
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-03-12 22:40:17 +0530

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -3,12 +3,55 @@ import './style.css';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 export default function Form() {
+
+  let [FormTemplate, setFormTemplate] = useState({
+    name: '',
+    age: '',
+    gender: '',
+    phnumber: '',
+    mail: '',
+    dept: ''
+  });
+
+
+  function submit()
+  {
+    alert(234567)
+  }
+
   return (
     <div>
-      <h1>This is Form page</h1>
       <br/>
       <br/>
       <Link to="/">Home</Link>
+      <br/>
+      <br/>
+      <h2>Form Page</h2>
+      <form action="" onSubmit={submit}>
+      <input type="text" name="name" placeholder="Name"/>
+      <br/>
+      <br/>
+      <input type="number" name="age" placeholder="Age"/>
+      <br/>
+      <br/>
+      <input type="text" name="gender" placeholder="Gender"/>
+      <br/>
+      <br/>
+      <input type="text" name="phnumber" placeholder="Contact Number"/>
+      <br/>
+      <br/>
+      <input type="mail" name="mail" placeholder="Mail ID"/>
+      <br/>
+      <br/>
+      <input type="text" name="dept" placeholder="Department"/>
+      <br/>
+      <br/>
+      <button type="submit">Submit</button>
+
+
+
+      </form>
+
     </div>
   );
 }
