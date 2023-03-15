b8a0d963f13af2d406af88336d63eecaa763d812
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-03-15 17:29:27 +0530

    1 file modified

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
+
 export default function DynamicForms() {
+
+  let [Dy_Form, setDy_Form] = useState([
+    {
+      fname : '',
+      lname : ''
+    },
+  ])
+
+  function handleSubmit()
+  {
+
+  }
+
+  
+
   return (
     <div>
       <br/>
@@ -11,7 +27,28 @@ export default function DynamicForms() {
       <Link to="/">Home</Link>
       <br/>
       <br/>
-      <input type="text"/>
+      <form action="" onSubmit={handleSubmit}>
+
+      {Dy_Form.map((ele,index)=>{
+        return(
+          <div key={index}>
+            <input type="text" name="fname" value={ele.fname} placeholder="First name"/>
+            <input type="text" name="lname" value={ele.lname} placeholder="First name"/>
+                {index === Dy_Form.length - 1 ? (
+                  <button onClick={addElement}>Add</button>
+                ) : (
+                  ''
+                )}
+                {index > 0 ? (
+                  <button onClick={() => removeElement(index)}>Remove</button>
+                ) : (
+                  ''
+                )}
+          </div>
+        )
+      })}
+
+      </form> 
     </div>
   );
 }
