458d7894fc1545b177a408ffb512ad1908be04da
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-04-01 15:42:52 +0530

    1 file modified

--- src/Career.js
+++ src/Career.js
@@ -3,9 +3,17 @@ import './style.css';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 export default function Career() {
+
+let navigate=useNavigate()
+function goto()
+{
+  navigate('/')
+}
+
   return (
     <div>
       <h1>This is Career page</h1>
+      <button onClick={goto}>Navigate through button instead of Link</button>
       <Link to="/">Home</Link>
       <br/>
       <br/>
